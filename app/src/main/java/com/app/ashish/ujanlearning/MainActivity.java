e5ca12085fe3ce01276732d9722c1349b14605f1
root <root@858b5933-43d8-444b-94e9-c9119bd501d5>
2015-02-01 20:01:45 +0000

    Basic screen

--- app/src/main/java/com/app/ashish/ujanlearning/MainActivity.java
+++ app/src/main/java/com/app/ashish/ujanlearning/MainActivity.java
@@ -1,9 +1,14 @@
 package com.app.ashish.ujanlearning;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +17,27 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String alphabet[][] = {{"A","B","C","D"}, {"E","F","G","H"},
+                                {"I","J","K","L"}, {"M","N","O","P"},
+                                {"Q","R","S","T"}, {"U","V","W","X"},
+                                {"Y","Z"}};
+        TableLayout tableLayout = (TableLayout)findViewById(R.id.alphabet);
+
+        for(int i = 0 ; i < alphabet.length; i++) {
+            TableRow row = new TableRow(this);
+            row.setBackgroundColor(Color.GRAY);
+            for(int j = 0; j < alphabet[i].length; j++) {
+                TextView textView = new TextView(this);
+                textView.setText(alphabet[i][i]);
+                if(j % 2 == 0) {
+                    textView.setBackgroundColor(Color.WHITE);
+                }
+                textView.setGravity(Gravity.CENTER);
+                row.addView(textView);
+            }
+            tableLayout.addView(row);
+        }
     }
 
 

d47b7cb7a664970122851088601e44e896b58fc4
root <root@858b5933-43d8-444b-94e9-c9119bd501d5>
2015-02-06 08:16:16 +0000

    Added animation in picture display and added another page to list down what all to be displayed

--- app/src/main/java/com/app/ashish/ujanlearning/MainActivity.java
+++ app/src/main/java/com/app/ashish/ujanlearning/MainActivity.java
@@ -1,89 +1,43 @@
 package com.app.ashish.ujanlearning;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.InputStream;
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RadioButton radioButton = (RadioButton)findViewById(R.id.radio_eng_caps);
+        radioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), EnglishCapsActivity.class);
+                startActivity(intent);
 
-        String alphabet[][] = {{"A","B","C","D"}, {"E","F","G","H"},
-                                {"I","J","K","L"}, {"M","N","O","P"},
-                                {"Q","R","S","T"}, {"U","V","W","X"},
-                                {"","Y","Z",""}};
-        TableLayout tableLayout = (TableLayout)findViewById(R.id.alphabet);
-        ImageView imageView = (ImageView)findViewById(R.id.imageView_grid);
-        imageView.setVisibility(View.INVISIBLE);
-
-        for(int i = 0 ; i < alphabet.length; i++) {
-            TableRow row = new TableRow(this);
-            row.setBackgroundColor(Color.GRAY);
-            row.setMinimumHeight(70);
-            for(int j = 0; j < alphabet[i].length; j++) {
-                final TextView textView = new TextView(this);
-
-                textView.setText(alphabet[i][j]);
-
-                //textView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-                textView.setTextSize(60);
-                if((i+j) % 2 == 0) {
-                    textView.setBackgroundColor(Color.WHITE);
-                }
-                textView.setGravity(Gravity.CENTER);
-
-                textView.setOnClickListener(new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v){
-
-                        ImageView imageView = (ImageView)findViewById(R.id.imageView_grid);
-//                        Bitmap bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
-                        final String path = Environment.getRootDirectory().getPath() + "/res/alphabet_a.jpg";
-//                        Toast.makeText(getApplicationContext(),textView.getText() + "- " + path ,Toast.LENGTH_SHORT).show();
-                        try {
-                            InputStream si1 = getAssets().open("english_" + textView.getText().toString().toLowerCase() + ".jpg");
-                            Bitmap image = BitmapFactory.decodeStream(si1);
-                            Bitmap scaledImage = Bitmap.createScaledBitmap(image, imageView.getWidth(), imageView.getHeight(), true);
-                            imageView.setImageBitmap(scaledImage);
-                            imageView.setVisibility(View.VISIBLE);
-                        } catch(Exception e) {
-                            e.printStackTrace();
-                        }
-//                        imageView.setImageResource(R.drawable.alphabet_a);
-                        imageView.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                ImageView imageView = (ImageView)findViewById(R.id.imageView_grid);
-                                imageView.setVisibility(View.INVISIBLE);
-                            }
-                        });
-                    }
-                });
-
-                row.addView(textView);
             }
-            tableLayout.addView(row);
-        }
+        });
     }
 
 
